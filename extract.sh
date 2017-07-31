572e7714ddddd7cd7b83efc97bc07d29ea29920e
Leonti Bielski <leonti.bielski@myob.com>
2017-07-31 12:54:43 +1000

    Download files from s3

--- extract.sh
+++ extract.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+
+current_date=$(date +"%y-%-m-%-d")
+
+mkdir /root/reaResults
 cd /root/reaResults
+
+aws s3 cp "s3://leonti-rea-crawler/"$current_date".zip" .
 
 for i in *; do
     unzip "$i"
     rm "$i"
 done
 
+mkdir /root/reaSoldResults
 cd /root/reaSoldResults
+
+aws s3 cp "s3://leonti-rea-crawler/"$current_date"-sold.zip" .
 
 for i in *; do
     unzip "$i"
