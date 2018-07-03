aa7e5337469f05dea8f83cc09c32b79c5eb837de
Leonti Bielski <leonti.bielski@myob.com>
2018-07-03 13:16:37 +1000

    Add travis integration

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ set -e
 
 version=$(date +"%y.%m.%d.%H.%M")
 
-sudo docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.04.04.03.12
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.04.04.03.12
 
-sudo docker build -t leonti/rea-extract:$version .
-sudo docker push leonti/rea-extract:$version
+docker build -t leonti/rea-extract:$version .
+docker push leonti/rea-extract:$version
 
 echo $version" is built"
