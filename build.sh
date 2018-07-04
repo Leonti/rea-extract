ffce911f7dc1c780247706534898a63414118c60
Leonti Bielski <leonti.bielski@myob.com>
2018-07-04 13:07:01 +1000

    Use new version of haskell-static-build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 version=$(date +"%y.%m.%d.%H.%M")
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.07.04.02.33
+docker run -v "$(pwd):/src" -v /tmp/stack:/root/.stack --rm leonti/haskell-static-build:18.07.04.03.03
 
 docker build -t leonti/rea-extract:$version .
 docker push leonti/rea-extract:$version
